9cdda9fa5e194729b507100ba8dbf1a120a5a152
Alessandro Fusco <alessandro.fusco3@studio.unibo.it>
2017-04-05 13:41:10 +0100

    VertexCover

--- test/EdgeGraph_.java
+++ test/EdgeGraph_.java
@@ -1,22 +1,21 @@
+/**
+ * @author afusco
+ * @date 04/04/17
+ */
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.*;
 
-public class Graph_ {
+public class EdgeGraph_ {
     @Rule
     public ExpectedException exception = ExpectedException.none();
 
-    @Test
-    public void should_not_create_a_graph_with_less_than_0_vertex() throws Exception {
-        exception.expect(IllegalArgumentException.class);
-        new Graph(-1);
-    }
 
     @Test
     public void should_create_an_empty_graph() throws Exception {
-        Graph graph = new Graph(0);
+        EdgeGraph graph = new EdgeGraph(0);
         assertTrue(graph.isEmpty());
         assertEquals(graph.size(), 0);
         assertEquals(graph.edges() , 0);
@@ -30,7 +29,7 @@ public class Graph_ {
     @Test
     public void should_create_a_graph_of_a_given_size() throws Exception {
         int size = 5;
-        Graph graph = new Graph(size);
+        EdgeGraph graph = new EdgeGraph(size);
         assertEquals(graph.size()    , size);
         assertFalse(graph.containsVertex(size));
         assertFalse(graph.containsVertex(-1)  );
@@ -40,7 +39,7 @@ public class Graph_ {
 
     @Test
     public void should_add_edges_between_existing_vertices() throws Exception {
-        Graph graph = new Graph(3);
+        EdgeGraph graph = new EdgeGraph(3);
         assertTrue(graph.addEdge(0, 0));
         assertTrue(graph.addEdge(0, 1));
         assertTrue(graph.addEdge(0, 2));
@@ -50,7 +49,7 @@ public class Graph_ {
 
     @Test
     public void should_not_add_an_edge_if_already_exists() throws Exception {
-        Graph graph = new Graph(3);
+        EdgeGraph graph = new EdgeGraph(3);
         assertTrue(graph.addEdge(0, 1));
         assertFalse(graph.addEdge(0, 1));
         assertEquals(1, graph.edges());
@@ -60,11 +59,33 @@ public class Graph_ {
     @Test
     public void should_not_add_edges_if_a_vertex_not_exists() throws Exception {
         exception.expect(IndexOutOfBoundsException.class);
-        new Graph(7).addEdge(0, 10);
+        new EdgeGraph(7).addEdge(0, 10);
     }
 
-    private Graph setupGraph(int size) {
-        Graph graph = new Graph(size);
+    @Test
+    public void should_remove_a_vertex_with_its_edges() throws Exception {
+        int size = 3;
+        EdgeGraph graph = setupEdgeGraph(size);
+        assertEquals(size, graph.size());
+        assertEquals(6, graph.edges());
+        assertTrue(graph.removeVertex(0));
+        assertEquals(size - 1, graph.size());
+        assertEquals(3, graph.edges());
+    }
+
+    @Test
+    public void should_not_remove_a_vertex_if_not_exists() throws Exception {
+        int size = 3;
+        EdgeGraph graph = setupEdgeGraph(size);
+        assertEquals(size, graph.size());
+        assertEquals(6, graph.edges());
+        assertFalse(graph.removeVertex(9));
+        assertEquals(size, graph.size());
+        assertEquals(6, graph.edges());
+    }
+
+    private EdgeGraph setupEdgeGraph(int size) {
+        EdgeGraph graph = new EdgeGraph(size);
         graph.addEdge(0, 0);
         graph.addEdge(0, 1);
         graph.addEdge(0, 2);
@@ -76,26 +97,17 @@ public class Graph_ {
 
     @Test
     public void should_build_a_copy_by_constructor() throws Exception {
-        Graph graph1 = setupGraph(3);
-        Graph graph2 = new Graph(graph1);
+        EdgeGraph graph1 = setupEdgeGraph(3);
+        EdgeGraph graph2 = new EdgeGraph(graph1);
         assertFalse(graph1 == graph2);
         assertEquals(graph1, graph2);
     }
 
-    @Test
-    public void summary_of_a_graph() throws Exception {
-        Graph graph = new Graph(3);
-        assertEquals("Graph { 3 vertices; 0 amountOfEdges }",  graph.toString());
-        graph.addEdge(0, 0);
-        graph.addEdge(0, 1);
-        graph.addEdge(0, 2);
-        graph.addEdge(1, 2);
-        assertEquals("Graph { 3 vertices; 4 amountOfEdges }", graph.toString());
-    }
+
 
     @Test
     public void should_add_vertices() throws Exception {
-        Graph g = new Graph();
+        EdgeGraph g = new EdgeGraph(0);
         assertEquals(0, g.size());
 
         assertTrue(g.addVertex(10));
@@ -108,6 +120,38 @@ public class Graph_ {
 
         assertTrue(g.addVertex(-1));
         assertEquals(2, g.size());
+    }
+
+    @Test
+    public void should_remove_vertices() throws Exception {
+        EdgeGraph g = new EdgeGraph(10);
+
+        assertTrue(g.removeVertex(1));
+        assertFalse(g.containsVertex(1));
+        assertEquals(9, g.size());
+
+        assertFalse(g.removeVertex(1));
+        assertFalse(g.containsVertex(1));
+    }
+
+    @Test
+    public void should_remove_adjacent_edges_when_removing_vertex() throws Exception {
+
+        EdgeGraph g = new EdgeGraph(3);
+
+        g.addEdge(0, 1);
+        g.addEdge(0,2);
+        assertEquals(2, g.edges());
+        assertEquals(3, g.size());
+        assertEquals(2, g.getDegree(0));
+        assertEquals(1, g.getDegree(1));
+        assertEquals(1, g.getDegree(2));
+
+        g.removeVertex(0);
+        assertEquals(0, g.edges());
+        assertEquals(2, g.size());
+        assertEquals(0, g.getDegree(1));
+        assertEquals(0, g.getDegree(2));
     }
 
 }
--- test/VertexCover_.java
+++ test/VertexCover_.java
@@ -1,3 +1,59 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Random;
 
 public class VertexCover_ {
+
+    Random r = new Random(1000);
+
+    @Test
+    public void should_test_valid_cover() throws Exception {
+
+        Graph g = new Graph(6);
+        g.addEdge(0,1);
+        g.addEdge(0,2);
+        g.addEdge(1,2);
+        g.addEdge(1,3);
+        g.addEdge(1,4);
+        g.addEdge(1,5);
+
+        ArrayList<Integer> cover = new ArrayList<Integer>();
+        cover.add(1);
+        cover.add(2);
+
+
+        assertTrue(VertexCover.isValidCover(g, cover));
+
+    }
+
+    @Test
+    public void should_test_vertex_cover() throws Exception {
+        Graph g = new Graph(6);
+        g.addEdge(0,1);
+        g.addEdge(0,2);
+        g.addEdge(1,2);
+        g.addEdge(1,3);
+        g.addEdge(1,4);
+        g.addEdge(1,5);
+
+        Collection<Integer> cover = VertexCover.cover(g);
+
+        assertTrue(VertexCover.isValidCover(g, cover));
+
+    }
+
+
+    @Test
+    public void should_test_vertex_cover2() throws Exception {
+
+        for (int i = 1; i <= 10; i++) {
+            Graph g = new Graph(100*i, 130*i, r);
+            Collection<Integer> cover = VertexCover.cover(g);
+            assertTrue(VertexCover.isValidCover(g, cover));
+        }
+
+    }
 }
